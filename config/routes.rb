e28384470d9141357238008ae8321fc1d369ae56
yangyepeng <yangyepeng@gmail.com>
2017-08-10 22:18:18 +0800

    complete 2 ajax on rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  resources :posts do
+    member do
+      post "like" => "posts#like"
+      post "unlike" => "posts#unlike"
+    end
+  end
+
   devise_for :users
   get "/jquery-1" => "pages#jquery_1"
   get "/jquery-2" => "pages#jquery_2"
@@ -6,6 +14,7 @@ Rails.application.routes.draw do
   get "/jquery-4" => "pages#jquery_4"
   get "/jquery-5" => "pages#jquery_5"
 
-  root "pages#jquery_1"
+  # root "pages#jquery_1"
+  root "posts#index"
 
 end
